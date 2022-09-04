5b0a65a9c1f7faeb6d27870bebd5bf9dbc8de311
Tak-jae-ho <twogh1emd@nate.com>
2022-09-05 03:59:09 +0900

    commit

--- code/util.py
+++ code/util.py
@@ -44,7 +44,8 @@ def plot_image_grid(output, img_size, n_row, epoch, sample_folder, result_dir=No
         if not os.path.exists(result_dir):
             os.mkdir(result_dir)
         fig.savefig(result_dir+'/epoch_%d' %(epoch))
-		plt.close('all')
+		
+		fig.close('all')
         pass
 
 def plot_curve_error(data_mean, data_std, x_label, y_label, title, file_name,result_dir=None):
@@ -68,7 +69,7 @@ def plot_curve_error(data_mean, data_std, x_label, y_label, title, file_name,res
         if not os.path.exists(result_dir):
             os.mkdir(result_dir)
         fig.savefig(result_dir + '/' + file_name)
-
+		
 		fig.close('all')
         pass
 
@@ -97,7 +98,7 @@ def plot_curve_error2(data1_mean, data1_std, data1_label, data2_mean, data2_std,
         if not os.path.exists(result_dir):
             os.mkdir(result_dir)
         fig.savefig(result_dir + '/' + file_name)
-
+		
 		fig.close('all')
         pass
 
